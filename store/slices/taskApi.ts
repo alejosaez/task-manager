cfebf0cf986ded65347bf3f10ad6f99f31b1662f
Alejo Saez Gebicki <alejosaezgebicki@gmail.com>
2024-12-29 17:24:04 -0300

    test deploy

--- store/slices/taskApi.ts
+++ store/slices/taskApi.ts
@@ -1,51 +1,36 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { Task } from "@/types/task";
-
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { Task } from '@/types/task';
 
 export const taskApi = createApi({
-  reducerPath: "taskApi",
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
-  tagTypes: ["Task"],
+  reducerPath: 'taskApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://task-manager-be-ihw3.onrender.com' }),
+  tagTypes: ['Task'],
   endpoints: (builder) => ({
     getTasks: builder.query<Task[], void>({
-      query: () => "/tasks",
-      providesTags: ["Task"],
+      query: () => '/tasks',
+      providesTags: ['Task'],
     }),
     getTaskById: builder.query<Task, string>({
       query: (id) => `/tasks/${id}`,
-      providesTags: (result, error, id) => [{ type: "Task", id }],
+      providesTags: (result, error, id) => [{ type: 'Task', id }],
     }),
-    updateTask: builder.mutation<
-      Task,
-      { id: string; title: string; description?: string; completed: boolean }
-    >({
+    updateTask: builder.mutation<Task, { id: string; title: string; description?: string; completed: boolean }>({
       query: ({ id, ...body }) => ({
         url: `/tasks/${id}`,
-        method: "PUT",
+        method: 'PUT',
         body,
       }),
-      invalidatesTags: (result, error, { id }) => [
-        { type: "Task", id },
-        { type: "Task" },
-      ],
+      invalidatesTags: (result, error, { id }) => [{ type: 'Task', id }, { type: 'Task' }],
     }),
-    createTask: builder.mutation<Task, { title: string; description?: string }>(
-      {
-        query: (body) => ({
-          url: "/tasks",
-          method: "POST",
-          body,
-        }),
-        invalidatesTags: ["Task"],
-      }
-    ),
+    createTask: builder.mutation<Task, { title: string; description?: string }>({
+      query: (body) => ({
+        url: '/tasks',
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: ['Task'],
+    }),
   }),
 });
 
-export const {
-  useGetTasksQuery,
-  useGetTaskByIdQuery,
-  useUpdateTaskMutation,
-  useCreateTaskMutation,
-} = taskApi;
+export const { useGetTasksQuery, useGetTaskByIdQuery, useUpdateTaskMutation, useCreateTaskMutation } = taskApi;
